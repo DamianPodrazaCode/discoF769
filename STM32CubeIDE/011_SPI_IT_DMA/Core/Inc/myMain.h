eee66673a8df48cf2faa97911c13c2f4a9192879
DamianPodrazaCode <damian.podraza@gmail.com>
2023-08-08 20:38:15 +0200

    Signed-off-by: DamianPodrazaCode <damian.podraza@gmail.com>

---
+++ STM32CubeIDE/011_SPI_IT_DMA/Core/Inc/myMain.h
@@ -0,0 +1,16 @@
+/*
+ * myMain.h
+ *
+ *  Created on: Aug 7, 2023
+ *      Author: ZoMbiE
+ */
+
+#ifndef INC_MYMAIN_H_
+#define INC_MYMAIN_H_
+
+#include "main.h"
+
+void setup();
+void loop();
+
+#endif /* INC_MYMAIN_H_ */
